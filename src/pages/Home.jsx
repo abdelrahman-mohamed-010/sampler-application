719e56982f38891dea49ffd9ab05a49befb10044
body <abdo.mhamed122yahoo.com@gmail.com>
2024-12-15 15:45:05 +0200

    get excel file from user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import * as XLSX from "xlsx";
 
 const Home = () => {
+  // const [file, setFile] = useState(null);
+  const [tableData, setTableData] = useState(null);
+
+  const handleFileUpload = (event) => {
+    const uploadedFile = event.target.files[0];
+    if (uploadedFile) {
+      // Read and parse Excel file
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        // Parse the file
+        const workbook = XLSX.read(e.target.result, { type: "binary" });
+
+        // Get the first sheet name
+        const sheetName = workbook.SheetNames[0];
+
+        // Convert sheet to JSON
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+        // Create file info object
+        const fileInfo = {
+          name: uploadedFile.name,
+          type: uploadedFile.type,
+          size: uploadedFile.size,
+          lastModified: uploadedFile.lastModified,
+          sheets: workbook.SheetNames,
+          data: {
+            [sheetName]: jsonData,
+          },
+        };
+
+        setTableData(fileInfo);
+      };
+
+      // Read the file as binary string
+      reader.readAsBinaryString(uploadedFile);
+    }
+  };
+
+  const handleImportClick = () => {
+    document.getElementById("fileInput").click();
+  };
+
+  console.log(tableData);
+
   return (
     <section id="firstSection">
       <div className="flex flex-col items-center justify-center relative select-none">
@@ -10,7 +58,18 @@ const Home = () => {
           className="w-[565px] h-[565px]"
         />
         <div className="flex flex-col absolute bottom-[-50px]">
-          <button className="w-[288px] h-[70px] mb-[10px] text-center cursor-pointer bg-[#189ab4] text-white font-bold rounded-[5px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] transition-all ease-in-out duration-300 hover:bg-[#0056b3]">
+          {/* Hidden input for file upload */}
+          <input
+            id="fileInput"
+            type="file"
+            accept=".xls,.xlsx,.csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,text/csv"
+            className="hidden"
+            onChange={handleFileUpload}
+          />
+          <button
+            onClick={handleImportClick}
+            className="w-[288px] h-[70px] mb-[10px] text-center cursor-pointer bg-[#189ab4] text-white font-bold rounded-[5px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] transition-all ease-in-out duration-300 hover:bg-[#0056b3]"
+          >
             Import File
           </button>
           <Link
