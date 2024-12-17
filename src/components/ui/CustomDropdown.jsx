1e433d467c2125aea6bba1f92a01d6111df45b05
body <abdo.mhamed122yahoo.com@gmail.com>
2024-12-17 03:53:20 +0200

    fix dropdown bug

--- src/components/ui/CustomDropdown.jsx
+++ src/components/ui/CustomDropdown.jsx
@@ -39,7 +39,7 @@ export const CustomDropdown = ({
         `}
         onClick={() => !isEditable && setIsOpen(!isOpen)}
       >
-        <span className={`${isOpen ? "text-[#8E8D8D]" : " text-dark"}`}>
+        <span className={`${isOpen || isEditable ? "text-[#8E8D8D]" : " text-dark"}`}>
           {selectedOption?.label || placeholder}
         </span>
         {!isEditable && (
